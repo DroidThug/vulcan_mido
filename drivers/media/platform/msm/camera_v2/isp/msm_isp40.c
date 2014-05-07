d5f5ced34a62a99e1860b2ea8fc3ba052a74184e
Shuzhen Wang <shuzhenw@codeaurora.org>
2014-05-07 12:18:30 -0700

    msm: camera: Directly use stride from buffer_plane_info
    stride is already in the unit of bytes. Just need to convert
    it to units of 64-bit words.
    
    Bug: 14395767
    Change-Id: I57960371911c492cab7d5ca5f710ca0f16572d77
    Signed-off-by: Shuzhen Wang <shuzhenw@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
@@ -1554,10 +1554,8 @@ static void msm_vfe40_axi_cfg_wm_reg(
 		msm_camera_io_w(val, vfe_dev->vfe_base + wm_base + 0x14);
 
 		/*WR_BUFFER_CFG*/
-		val =
-			msm_isp_cal_word_per_line(stream_info->output_format,
-			stream_info->plane_cfg[
-				plane_idx].output_stride) << 16 |
+		val = (stream_info->plane_cfg[
+				plane_idx].output_stride/8) << 16 |
 			(stream_info->plane_cfg[
 				plane_idx].output_height - 1) << wm_bit_shift |
 			burst_len;
