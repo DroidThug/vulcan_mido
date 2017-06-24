309cdce22d1928b61857998731b222a39a8bf096
Morgan Lee <morganleezd2003@googlemail.com>
2017-06-24 23:36:22 +0800

    build script: Use linaro-7.1 TC
     Author:    Morgan Lee <morganleezd2003@googlemail.com>
     Date:      Sat Jun 24 23:36:22 2017 +0800

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ export SUBARCH=arm64
 export KBUILD_BUILD_USER="alamo"
 export KBUILD_BUILD_HOST="bendyland"
 #export CROSS_COMPILE=/home/alamo/toolchains/aarch64-linux-android-linaro-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE=/home/alamo/toolchains/aarch64-uber-linux-android-4.9.4/bin/aarch64-linux-android-
+#export CROSS_COMPILE=/home/alamo/toolchains/aarch64-uber-linux-android-4.9.4/bin/aarch64-linux-android-
 #export CROSS_COMPILE=/home/alamo/toolchains/aarch64-linaro-linux-android-6.3.1/bin/aarch64-linux-android-
 #export CROSS_COMPILE=/home/alamo/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/alamo/toolchains/linaro-7/bin/aarch64-linaro-linux-gnu-
 
 # Cleanup
 make clean
