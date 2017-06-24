e45ff01c888b8f5e3caac8fe3c2bf0ad530de798
Morgan <morganleezd2003@gmail.com>
2017-06-24 07:20:28 +0530

    Revert "mmc/core: implement sysfs variable to switch CRC off / on"
    This reverts commit 08b49bac50027d676bad5e2a1522d96dd4205a9a.

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -66,12 +66,15 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  * Enabling software CRCs on the data blocks can be a significant (30%)
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
- */ bool use_spi_crc = 0; EXPORT_SYMBOL(use_spi_crc); 
-module_param_named(crc, use_spi_crc, bool, 0644); /* MODULE_PARM_DESC( * 
-Internal function. Schedule delayed work in the MMC work queue.
-	crc, */ static int mmc_schedule_delayed_work(struct delayed_work 
-*work,
-	"Enable/disable CRC checking");				     unsigned long delay)
+ */
+bool use_spi_crc = 0;
+module_param(use_spi_crc, bool, 0);
+
+/*
+ * Internal function. Schedule delayed work in the MMC work queue.
+ */
+static int mmc_schedule_delayed_work(struct delayed_work *work,
+				     unsigned long delay)
 {
 	return queue_delayed_work(workqueue, work, delay);
 }
