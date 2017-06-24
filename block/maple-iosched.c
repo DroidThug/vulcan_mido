e8018539629737091766095dc8571c98e5fedea5
Morgan Lee <morganleezd2003@gmail.com>
2017-06-24 18:50:53 +0800

    block: maple: Remove CONFIG_HZ dependency, assume 300 for interrupt freq

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -24,10 +24,10 @@
 enum { ASYNC, SYNC };
 
 /* Tunables */
-static const int sync_read_expire = 100;	/* max time before a read sync is submitted. */
-static const int sync_write_expire = 350;	/* max time before a write sync is submitted. */
-static const int async_read_expire = 200;	/* ditto for read async, these limits are SOFT! */
-static const int async_write_expire = 500;	/* ditto for write async, these limits are SOFT! */
+static const int sync_read_expire = 150;	/* max time before a read sync is submitted. */
+static const int sync_write_expire = 150;	/* max time before a write sync is submitted. */
+static const int async_read_expire = 900;	/* ditto for read async, these limits are SOFT! */
+static const int async_write_expire = 900;	/* ditto for write async, these limits are SOFT! */
 static const int fifo_batch = 16;		/* # of sequential requests treated as one by the above parameters. */
 static const int writes_starved = 3;		/* max times reads can starve a write */
 static const int sleep_latency_multiple = 5;	/* multple for expire time when device is asleep */
