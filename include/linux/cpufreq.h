3f13a3c993a2acfffaa7d5813383fd4fa4035a1f
alamo18 <morganleezd2003@googlemail.com>
2017-07-28 01:21:29 +0800

    cpufreq: Add zzmoove governor Backlight might need suspend resume

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -522,6 +522,9 @@ extern struct cpufreq_governor cpufreq_gov_nebula;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELLSACTIVE)
 extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_hellsactive)
